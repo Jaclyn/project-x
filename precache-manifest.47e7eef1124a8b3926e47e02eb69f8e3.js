5c31bc379ab116fdd2e4f8dff6d2cc8ec3941b0c
jaclyn <xinman890820@hotmail.com>
2019-03-03 12:56:27 +0800

    Updates

---
+++ precache-manifest.47e7eef1124a8b3926e47e02eb69f8e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "684a8a690e97ab8ff765",
+    "url": "/project-x/static/css/main.cd97e8e7.chunk.css"
+  },
+  {
+    "revision": "684a8a690e97ab8ff765",
+    "url": "/project-x/static/js/main.684a8a69.chunk.js"
+  },
+  {
+    "revision": "2b774f292e6e875eaced",
+    "url": "/project-x/static/js/runtime~main.2b774f29.js"
+  },
+  {
+    "revision": "85fda52ac54ef6f643e8",
+    "url": "/project-x/static/js/2.85fda52a.chunk.js"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/project-x/static/media/shape.208f3d38.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-x/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1bad8082d6a3990261576902910677ad",
+    "url": "/project-x/index.html"
+  }
+];
